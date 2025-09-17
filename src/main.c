16bd80b29b7bad22bc178c303da38dde26c5eb2b
Amit <gmate.amit@gmail.com>
2025-09-17 14:12:04 +0200

    enabling zephyr logging and adding .cache directory to ignore list

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 #define SLEEP_TIME_MS   1000
 int main(void) 
 {
+    LOG_INF("Entering main\n");
     while (1) 
     {
         k_msleep(SLEEP_TIME_MS);
